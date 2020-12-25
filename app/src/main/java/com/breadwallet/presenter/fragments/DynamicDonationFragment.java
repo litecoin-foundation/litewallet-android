205dc2e98bdc880c9590f692e907c35890325a9a
Mohamed Barry <mosadialiou@gmail.com>
2020-12-25 11:40:28 +0100

    DynamicDonationFragment: convert to android

--- app/src/main/java/com/breadwallet/presenter/fragments/DynamicDonationFragment.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/DynamicDonationFragment.java
@@ -1,6 +1,5 @@
 package com.breadwallet.presenter.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.LayoutInflater;
@@ -15,6 +14,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.entities.PaymentItem;
