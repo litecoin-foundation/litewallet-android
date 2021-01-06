67a34fe55213181756255e1486923446bb98f7c5
Mohamed Barry <mosadialiou@gmail.com>
2021-01-06 03:21:34 +0100

    FragmentSend: disable Lookup button until the domain string is at least 4 chars

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentSend.kt
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentSend.kt
@@ -16,6 +16,7 @@ import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.text.trimmedLength
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.transition.AutoTransition
@@ -311,6 +312,8 @@ class FragmentSend : Fragment() {
         })
 
         udLookupButton.setOnClickListener {
+            // Disable the button until the domain string is at least 4 chars long (e.g a.zil)
+            if (udDomainEdit.text.trimmedLength() < 4) return@setOnClickListener
             lifecycleScope.executeAsyncTask(
                     onPreExecute = {
                         udLookupButton.showProgress {
@@ -321,7 +324,7 @@ class FragmentSend : Fragment() {
                         addressEdit.text = null
                         AnalyticsManager.logCustomEventWithParams(BRConstants._20201121_SIL, Bundle().apply { putLong(BRConstants.START_TIME, System.currentTimeMillis()) })
                     },
-                    doInBackground = { UDResolution().resolve(udDomainEdit.text.toString()) },
+                    doInBackground = { UDResolution().resolve(udDomainEdit.text.trim().toString()) },
                     onPostExecute = {
                         if (it.error == null) {
                             AnalyticsManager.logCustomEventWithParams(BRConstants._20201121_DRIA, Bundle().apply { putLong(BRConstants.SUCCESS_TIME, System.currentTimeMillis()) })
