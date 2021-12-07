a32b4a16ededc2e13821007bb96f733ae96919d5
kcw-grunt <mrkerrywashington@icloud.com>
2021-12-07 00:19:24 +0000

    Refactor
    Removed BRHTTPHelper class
    Reduce cruft in APIClient
    -  Some methods being used
    - Will  reduce more in upcoming commits
    Removed the huge header
    Removed empty classes
    Removed excessive headers
    Remove references to BitID and BCH
    Renamed the GET url method
    Removed unused imports in APIClient
    Removed some KeyStore code

--- app/src/main/java/com/breadwallet/wallet/BRWalletManager.java
+++ app/src/main/java/com/breadwallet/wallet/BRWalletManager.java
@@ -65,31 +65,6 @@ import java.util.Locale;
 
 import timber.log.Timber;
 
-/**
- * BreadWallet
- * <p/>
- * Created by Mihail Gutan <mihail@breadwallet.com> on 12/10/15.
- * Copyright (c) 2016 breadwallet LLC
- * <p/>
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p/>
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * <p/>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 public class BRWalletManager {
     private static BRWalletManager instance;
     public List<OnBalanceChanged> balanceListeners = new ArrayList<>();
@@ -192,7 +167,7 @@ public class BRWalletManager {
         BRExecutor.getInstance().forLightWeightBackgroundTasks().execute(new Runnable() {
             @Override
             public void run() {
-                KVStoreManager.getInstance().putWalletInfo(ctx, info); //push the creation time to the kv store
+                //Removed the push to the KeyStore / Server that is refactored out of the Litewallet code
             }
         });
 
