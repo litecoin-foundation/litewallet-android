74dfd6dc85a122a0762021ab967af7dcdd2a769b
Kerry Washington <mrkerrywashington@icloud.com>
2022-05-24 23:13:22 +0100

    🚀[ Release v.2.6.0] Merge into Main  (#12)
    * Added український language
    
    - add uk xml file
    -  removed the nl, sv, da files
    - Escaped apostrphes in UK strings file
    - Updated language choices
    
    * 🥳[Feature] Unstoppable ext update (#11)
    
    * WIP: DNC
    
    Dropped in the UD code prior to testing
    
    * Added study link
    
    * Fix unstoppable domains dependency
    
    * Updated the UD ext text
    
    WIP::::Need to refactor once UK and deletion on SV, NL, DA
    
    * added UK lang
    
    Co-authored-by: Victor Sima <vic.sima@gmail.com>
    
    * version bump
    
    * Hotfix/crashes v2.6.0 (#17)
    
    * Added notes
    
    * Bugfix - add missing pendingIntent flag for android 13
    
    * Use firebase bom
    
    Co-authored-by: kcw-grunt <mrkerrywashington@icloud.com>
    
    Co-authored-by: Victor Sima <vic.sima@gmail.com>

--- app/src/main/java/com/breadwallet/entities/Language.kt
+++ app/src/main/java/com/breadwallet/entities/Language.kt
@@ -7,23 +7,20 @@ package com.breadwallet.entities
  * Copyright © 2021 Litecoin Foundation. All rights reserved.
  */
 enum class Language(val code: String, val title: String, val desc: String) {
-    DANISH("da", "Dansk", "Vælg sprog"),
     GERMAN("de", "Deutsch", "Sprache auswählen"),
     ENGLISH("en", "English", "Select language"),
     SPANISH("es", "Español", "Seleccione el idioma"),
     FRENCH("fr", "Français", "Sélectionner la langue"),
     INDONESIAN("in", "Indonesia", "Pilih bahasa"),
     ITALIAN("it", "Italiano", "Seleziona la lingua"),
-    DUTCH("nl", "Nederlands", "Selecteer taal"),
     PORTUGUESE("pt", "Português", "Selecione o idioma"),
-    SWEDISH("sv", "Svenska", "Välj språk"),
     TURKISH("tr", "Türkçe", "Dil Seçin"),
+    UKRAINIAN("uk", "Yкраїнський", "Оберіть мову"),
     RUSSIAN("ru", "Pусский", "Выберите язык"),
     KOREAN("ko", "한국어", "언어 선택"),
     JAPANESE("ja", "日本語", "言語を選択する"),
     CHINESE_SIMPLIFIED("zh-CN", "简化字", "选择语言"),
     CHINESE_TRADITIONAL("zh-TW", "繁體字", "選擇語言");
-
     companion object {
         fun find(code: String?): Language = values().find { it.code == code } ?: ENGLISH
     }
