8e772d5fc8761a4681dc58689c61097d3516eeb8
kcw-grunt <mrkerrywashington@icloud.com>
2022-10-29 11:48:21 +0100

    added Bitrefill web listener

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentBuy.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentBuy.java
@@ -19,6 +19,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
@@ -92,18 +93,37 @@ public class FragmentBuy extends Fragment {
         webSettings.setDomStorageEnabled(true);
         webSettings.setJavaScriptEnabled(true);
 
+        // App (in Java)
+        WebMessageListener bitrefillListener = new WebMessageListener() {
+            @Override
+            public void onPostMessage(WebView view, WebMessageCompat message, Uri sourceOrigin,
+                                      boolean isMainFrame, JavaScriptReplyProxy replyProxy) {
+                // do something about view, message, sourceOrigin and isMainFrame.
+                replyProxy.postMessage("Got it!");
+            }
+        };
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_MESSAGE_LISTENER)) {
+            WebViewCompat.addWebMessageListener(webView, "bitrefillPostObj", rules, bitrefillListener);
+        }
+
 
         String currency = getArguments().getString(CURRENCY_KEY);
         Partner partner = (Partner) getArguments().getSerializable(PARTNER_KEY);
 
-        String buyUrl = partner == Partner.BITREFILL ? BRConstants.BITREFILL_AFFILIATE_LINK : url(getContext(), partner, currency);
+        String bitrefillRef = "bAshL935";
+        String utmSource = "LitewalletAndroid";
+        String bitrefillUrl = String.format( BRConstants.BITREFILL_AFFILIATE_LINK + "/embed/?paymentMethod=litecoin&ref=%s&utm_source=%s", bitrefillRef,utmSource);
+
+        String buyUrl = partner == Partner.BITREFILL ? bitrefillUrl : url(getContext(), partner, currency);
  
         Timber.d("URL %s", buyUrl);
         webView.loadUrl(buyUrl);
+
         return rootView;
     }
 
-    public static String url(Context context, Partner partner, String currency) {
+
+        public static String url(Context context, Partner partner, String currency) {
         String walletAddress = BRSharedPrefs.getReceiveAddress(context);
         Long timestamp = new Date().getTime();
         String uuid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
