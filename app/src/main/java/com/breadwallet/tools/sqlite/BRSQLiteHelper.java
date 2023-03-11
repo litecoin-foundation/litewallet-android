4934f2accb8515be6ecf745d915d7de120c2f752
kcw-grunt <mrkerrywashington@icloud.com>
2023-03-11 18:33:54 +0000

    Added Timber logs
    - added localizations
    - version and code bump
    - Removed the direct wallet info fetch
    - This call caused the app the reset the seed.
    - instead we are simply pulling the seed from the keystore
    - lean to call for seed

--- app/src/main/java/com/breadwallet/tools/sqlite/BRSQLiteHelper.java
+++ app/src/main/java/com/breadwallet/tools/sqlite/BRSQLiteHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.Date;
+
 import timber.log.Timber;
 
 public class BRSQLiteHelper extends SQLiteOpenHelper {
@@ -93,13 +95,17 @@ public class BRSQLiteHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         Timber.e("timber: Upgrading database from version " + oldVersion + " to "
-                + newVersion);
+                + newVersion + ", which will destroy all old data");
 
         // Clear DB tables to enable Bech32 features
         if (oldVersion == 12 && newVersion == 13) {
+            Timber.e("timber: Delete start %s", new Date().toString());
             db.execSQL("DELETE FROM " + MB_TABLE_NAME);
-            db.execSQL("DELETE FROM " + PEER_TABLE_NAME);
             db.execSQL("DELETE FROM " + TX_TABLE_NAME);
-        } 
+            db.execSQL("DELETE FROM " + PEER_TABLE_NAME);
+            db.execSQL("DELETE FROM " + CURRENCY_TABLE_NAME);
+            Timber.e("timber: Delete Finish %s", new Date().toString());
+
+        }
     }
 }
