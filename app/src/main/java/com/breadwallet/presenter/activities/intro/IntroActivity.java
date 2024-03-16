0cbbd5724512542167fb129678e5a332a9f36f70
josikie <kiejosi12@gmail.com>
2024-03-16 23:49:19 +0700

    Added Announcement view to encourage email mailing list

--- app/src/main/java/com/breadwallet/presenter/activities/intro/IntroActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/intro/IntroActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.breadwallet.presenter.activities.AnnounceUpdatesViewActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 import com.breadwallet.BuildConfig;
