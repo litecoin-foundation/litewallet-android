3479a2479eca561ddc7f3a8b750e497a9600b454
andhikayuana <andhikayuana@gmail.com>
2024-12-11 06:16:03 +0700

    chore: resolve CurrencyTests

--- app/src/test/java/com/litewallet/currency/CurrencyTests.kt
+++ app/src/test/java/com/litewallet/currency/CurrencyTests.kt
@@ -1,102 +1,99 @@
 package com.litewallet.currency
+
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import com.breadwallet.presenter.entities.CurrencyEntity
 import com.breadwallet.tools.sqlite.CurrencyDataSource
-import com.breadwallet.tools.util.LocaleHelper
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
-import io.mockk.verify
-import kotlinx.coroutines.delay
+import io.mockk.spyk
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
 
 class CurrencyTests {
 
+    private lateinit var database: SQLiteDatabase
+    private var currencyDataSource: CurrencyDataSource? = null
+
+    @Before
+    fun setup() {
+        val context: Context = mockk(relaxed = true)
+        currencyDataSource = spyk(CurrencyDataSource.getInstance(context))
+
+        database = mockk<SQLiteDatabase>()
+        every { currencyDataSource?.openDatabase() } returns database
+    }
+
+    @After
+    fun tearDown() {
+        currencyDataSource?.closeDatabase()
+        currencyDataSource = null
+    }
+
     @Test
     fun `invoke CurrencyDataSource instance and getAllCurrencies, should return the correct number of currencies`() {
-        val context: Context = mockk(relaxed = true)
-        val currencyDataSource = CurrencyDataSource.getInstance(context)
-        assertEquals(currencyDataSource.allCurrencies.count(), 20)
+        mockCursorDataFromDatabase()
+        assertEquals(currencyDataSource?.allCurrencies?.count(), 0)
     }
 
     @Test
     fun `invoke CurrencyDataSource instance and open database, then should validate database was not null`() {
-        val context: Context = mockk(relaxed = true)
-        val currencyDataSource = CurrencyDataSource.getInstance(context)
-        val database: SQLiteDatabase = currencyDataSource.openDatabase()
+        val database = currencyDataSource?.openDatabase()
         assertNotNull(database)
     }
 
     @Test
     fun `invoke CurrencyDataSource instance database, should return currency by ISO`() {
-        val context: Context = mockk(relaxed = true)
-        val currencyDataSource = CurrencyDataSource.getInstance(context)
-        val database: SQLiteDatabase = currencyDataSource.openDatabase()
 
-        val usdString = "USD"
-        assertEquals("USD", currencyDataSource.getCurrencyByIso(usdString))
+        val expected = CurrencyEntity("USD", "USD", 110.345f)
+        mockCursorDataFromDatabase(isEmpty = false, expected = expected)
 
-        val eurString = "EUR"
-        assertEquals("EUR", currencyDataSource.getCurrencyByIso(usdString))
-
-        val gbpString = "GBP"
-        assertEquals("GBP", currencyDataSource.getCurrencyByIso(usdString))
-
-        val rubString = "RUB"
-        assertEquals("RUB", currencyDataSource.getCurrencyByIso(usdString))
-
-        val uahString = "UAH"
-        assertEquals("UAH", currencyDataSource.getCurrencyByIso(usdString))
-
-        val idrString = "IDR"
-        assertEquals("IDR", currencyDataSource.getCurrencyByIso(usdString))
-
-        val jpyString = "JPY"
-        assertEquals("JPY", currencyDataSource.getCurrencyByIso(usdString))
-
-        val brlString = "BRL"
-        assertEquals("BRL", currencyDataSource.getCurrencyByIso(usdString))
-
-        val tryString = "TRY"
-        assertEquals("TRY", currencyDataSource.getCurrencyByIso(usdString))
+        val actual = currencyDataSource?.getCurrencyByIso("USD")
+        assertEquals(expected.code, actual?.code)
+        assertEquals(expected.name, actual?.name)
+        assertEquals(expected.rate, actual?.rate)
     }
 
     @Test
-    fun `add invoked with valid mocked value for fiatCurrency, should return correct value as expected`() {
+    fun `invoke putCurrencies, then should save the CurrencyEntity into database`() {
+        val fetchedCurrencies = listOf(
+            CurrencyEntity("USD", "USD", 110.345f),
+            CurrencyEntity("IDR", "IDR", 1752020.9450135066f)
+        )
 
-        val amountOne = mockk<Amount>()
-        val fiatCurrency = mockk<FiatCurrency>()
-        val context: Context = mockk(relaxed = true)
-
-        every { amountOne.value } returns 232432.42234
-        every { fiatCurrency.code } returns "USD"
-        every { fiatCurrency.valuePerLitecoin } returns 0.9899
-
-        val system = CurrencySystemUnderTest(fiatCurrency, amountOne)
-
-        val expected = 7
-        val inversionResult = system.invert()
-        assertEquals(expected, inversionResult)
-        verify {
-            amountOne.value
-            fiatCurrency.code
-            fiatCurrency.valuePerLitecoin
+        every { database.beginTransaction() } just Runs
+        fetchedCurrencies.forEachIndexed { index, currencyEntity ->
+            every { database.insertWithOnConflict(any(), any(), any(), any()) } returns (index + 1L)
         }
+        every { database.setTransactionSuccessful() } just Runs
+        every { database.endTransaction() } just Runs
+
+        currencyDataSource?.putCurrencies(fetchedCurrencies)
     }
-}
 
-class FiatCurrency(val code: String, val valuePerLitecoin: Double)
-class Amount(val value: Double)
+    private fun mockCursorDataFromDatabase(
+        isEmpty: Boolean = true,
+        expected: CurrencyEntity? = null
+    ) {
+        val cursor = mockk<Cursor>()
+        every { database.query(any(), any(), any(), any(), any(), any(), any()) } returns cursor
+        if (isEmpty) {
+            every { cursor.moveToFirst() } returns false
+            every { cursor.isAfterLast } returns true
+        } else {
+            every { cursor.getString(any()) } returns expected?.code
+            every { cursor.getString(any()) } returns expected?.name
+            every { cursor.getFloat(any()) } returns expected?.rate!!
 
-class CurrencySystemUnderTest(
-    private val fiatCurrency: FiatCurrency,
-    private val amount: Amount
-) {
-    fun invert(): Double = 1 / fiatCurrency.valuePerLitecoin
-
-    suspend fun fetch(): List<Double> {
-        delay(2000)
-        return listOf(fiatCurrency.valuePerLitecoin, invert())
+            every { cursor.moveToFirst() } returns true
+            every { cursor.isAfterLast } returns false
+        }
+        every { cursor.close() } just Runs
     }
 }
