6d43e9822d2b26def7787722fbc7b443103debc9
Kerry Washington <mrkerrywashington@icloud.com>
2024-12-12 20:43:36 +0000

    WIP
    Trying to mock testing of the key objects in the JSON
    Adding the GoogleService Tests

--- app/src/test/java/com/litewallet/tools/util/GoogleServicesTests.kt
+++ app/src/test/java/com/litewallet/tools/util/GoogleServicesTests.kt
@@ -1,43 +1,45 @@
 package com.litewallet.tools.util
-
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import java.io.File
-import java.io.FileReader
-import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
-
-import android.content.Context
-import com.breadwallet.entities.Language
-import com.breadwallet.tools.util.LocaleHelper
-import io.grpc.internal.JsonParser
-import io.mockk.mockk
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
-
-import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.Runs
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.spyk
-import io.mockk.verify
-import io.mockk.verifyOrder
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
-
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
 
 
 class GoogleServicesTest {
-    @Test
-    fun testGoogleServicesJson() {
-        // Load the app/google-services.json file
-        val file = File("app/google-services.json")
-        val json = JsonParser().parse(FileReader(file)).asJsonObject
 
-        // Test the project_info property
-        val projectInfo = json.getAsJsonObject("project_info")
-        assertEquals("litewallet-beta", projectInfo.get("project_id").asString)
-        assertEquals("230187998656", projectInfo.get("project_number").asString)
+    private lateinit var googleServicesJSONFile: File
+
+    @JvmField
+    @Rule
+    val temporaryFolder = TemporaryFolder()
+
+    @Before
+    fun setUp() {
+        // Create a temporary file with specific contents
+        googleServicesJSONFile = temporaryFolder.newFile("google-services.json")
+        googleServicesJSONFile.writeText("\"{\\n  \\\"project_info\\\": {\\n    \\\"project_number\\\": \\\"230187998656\\\",\\n    \\\"project_id\\\": \\\"litewallet-beta\\\"\\n  }\\n}\"")
+    }
+
+    @Test
+    fun `read GoogleServices file`() {
+
+    }
+    @Test
+    fun `read GoogleServices file and parse into a JSON object , should return the correct project_id`() {
+
+        //        val projectInfo = json.getAsJsonObject("project_info")
+        //        assertEquals("litewallet-beta", projectInfo.get("project_id").asString)
+    }
+
+    @Test
+    fun `read GoogleServices file and parse into a JSON object , should return the correct project_number`() {
+        //        val projectInfo = json.getAsJsonObject("project_info")
+        //        assertEquals("230187998656", projectInfo.get("project_number").asString)
     }
 }
