d396e383de5b68f39d73de0db5da4f15b819bcb2
andhikayuana <andhikayuana@gmail.com>
2024-12-14 07:22:32 +0700

    feat: implement simple cloud messaging

--- app/src/main/java/com/litewallet/notification/NotificationHandler.kt
+++ app/src/main/java/com/litewallet/notification/NotificationHandler.kt
@@ -1,24 +1,67 @@
 package com.litewallet.notification
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.breadwallet.R
-import com.litewallet.notification.NotificationHandler.Companion.NOTIFICATION_CHANNEL_ID_GENERAL
-import com.litewallet.notification.NotificationHandler.Companion.NOTIFICATION_CHANNEL_ID_LITECOIN_NEWS
-import com.litewallet.notification.NotificationHandler.Companion.NOTIFICATION_CHANNEL_ID_LITEWALLET_UPDATE
+import com.breadwallet.presenter.activities.BreadActivity
+import com.google.firebase.messaging.RemoteMessage
+import com.litewallet.notification.NotificationHandler.NOTIFICATION_CHANNEL_ID_GENERAL
+import com.litewallet.notification.NotificationHandler.NOTIFICATION_CHANNEL_ID_LITECOIN_NEWS
+import com.litewallet.notification.NotificationHandler.NOTIFICATION_CHANNEL_ID_LITEWALLET_UPDATE
 
-interface NotificationHandler {
+object NotificationHandler {
 
-    //todo
+    @SuppressLint("MissingPermission")
+    fun handleMessageReceived(context: Context, remoteMessage: RemoteMessage): Boolean {
+        if (remoteMessage.data.containsKey(KEY_DATA_LITEWALLET).not()) {
+            return false
+        }
 
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID_GENERAL = "general"
-        const val NOTIFICATION_CHANNEL_ID_LITECOIN_NEWS = "litecoin-news"
-        const val NOTIFICATION_CHANNEL_ID_LITEWALLET_UPDATE = "litewallet-update"
+        val channelId = remoteMessage.notification?.channelId ?: NOTIFICATION_CHANNEL_ID_GENERAL
+        val title = remoteMessage.data["title"] ?: return false
+        val body = remoteMessage.data["body"] ?: return false
+
+        if (defaultNotificationChannels.contains(channelId).not()) {
+            createNotificationChannel(context, channelId, channelId)
+        }
+
+        val intent = Intent(context, BreadActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.litewallet_logotype_white_notifications)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        NotificationManagerCompat.from(context)
+            .notify(System.currentTimeMillis().toInt(), notification.build())
+
+        return true
     }
+
+    const val KEY_DATA_LITEWALLET = "litewallet"
+
+    const val NOTIFICATION_CHANNEL_ID_GENERAL = "general"
+    const val NOTIFICATION_CHANNEL_ID_LITECOIN_NEWS = "litecoin-news"
+    const val NOTIFICATION_CHANNEL_ID_LITEWALLET_UPDATE = "litewallet-update"
+
+    val defaultNotificationChannels = setOf(
+        NOTIFICATION_CHANNEL_ID_GENERAL,
+        NOTIFICATION_CHANNEL_ID_LITECOIN_NEWS,
+        NOTIFICATION_CHANNEL_ID_LITEWALLET_UPDATE
+    )
 }
 
 fun setupNotificationChannels(context: Context) {
