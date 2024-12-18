7b99a74938678a117d40c5f192ebd1bef01318e1
josikie <kiejosi12@gmail.com>
2024-12-18 14:45:02 +0700

    AAdd tests BRConstants MockK+JUnit4

--- app/src/test/java/com/litewallet/tools/util/BRConstantsTest.kt
+++ app/src/test/java/com/litewallet/tools/util/BRConstantsTest.kt
@@ -23,6 +23,51 @@ class BRConstantsTest {
         assertSame(BRConstants.litecoinLowercase,"ł")
         assertSame(BRConstants.litecoinUppercase,"Ł")
     }
+
+    @Test
+    fun `validate App external URL constant`(){
+        assertSame(BRConstants.TWITTER_LINK,"https://twitter.com/Litewallet_App");
+        assertSame(BRConstants.INSTAGRAM_LINK,"https://www.instagram.com/litewallet.app");
+        assertSame(BRConstants.WEB_LINK,"https://litewallet.io");
+        assertSame(BRConstants.TOS_LINK,"https://litewallet.io/privacy");
+        assertSame(BRConstants.BITREFILL_AFFILIATE_LINK,"https://www.bitrefill.com/")
+        assertSame(BRConstants.CUSTOMER_SUPPORT_LINK,"https://support.litewallet.io/hc/en-us/requests/new");
+    }
+
+    @Test
+    fun `validate Firebase Analytics constants`(){
+        assertSame(BRConstants._20191105_AL,"app_launched");
+        assertSame(BRConstants._20191105_VSC,"visit_send_controller");
+        assertSame(BRConstants._20202116_VRC,"visit_receive_controller");
+        assertSame(BRConstants._20191105_DSL,"did_send_ltc");
+        assertSame(BRConstants._20191105_DTBT,"did_tap_buy_tab");
+        assertSame(BRConstants._20200111_RNI,"rate_not_initialized");
+        assertSame(BRConstants._20200111_FNI,"feeperkb_not_initialized");
+        assertSame(BRConstants._20200111_TNI,"transaction_not_initialized");
+        assertSame(BRConstants._20200111_WNI,"wallet_not_initialized");
+        assertSame(BRConstants._20200111_PNI,"phrase_not_initialized");
+        assertSame(BRConstants._20200111_UTST,"unable_to_sign_transaction");
+        assertSame(BRConstants._20200112_ERR,"lwa_error");
+        assertSame(BRConstants._20200112_DSR,"did_start_resync");
+        assertSame(BRConstants._20200125_DSRR,"did_show_review_request");
+        assertSame(BRConstants._20201118_DTGS,"did_tap_get_support");
+        assertSame(BRConstants._20200217_DUWP,"did_unlock_with_pin");
+        assertSame(BRConstants._20200217_DUWB,"did_unlock_with_biometrics");
+        assertSame(BRConstants._20200301_DUDFPK,"did_use_default_fee_per_kb");
+        assertSame(BRConstants._20201121_SIL,"started_IFPS_lookup");
+        assertSame(BRConstants._20201121_DRIA,"did_resolve_IPFS_address");
+        assertSame(BRConstants._20201121_FRIA,"failed_resolve_IPFS_address");
+        assertSame(BRConstants._20200207_DTHB,"did_tap_header_balance");
+        assertSame(BRConstants._20210427_HCIEEH,"heartbeat_check_if_event_even_happens");
+        assertSame(BRConstants._20220822_UTOU,"user_tapped_on_ud");
+        assertSame(BRConstants._20230131_NENR,"no_error_nominal_response");
+        assertSame(BRConstants._20230407_DCS,"did_complete_sync");
+        assertSame(BRConstants._20240123_RAGI,"registered_android_general_interest");
+        assertSame(BRConstants._20231225_UAP,"user_accepted_push");
+        assertSame(BRConstants._20240101_US,"user_signup");
+        assertSame(BRConstants._20241006_DRR,"did_request_rating");
+        assertSame(BRConstants._20241006_UCR,"user_completed_rating");
+    }
 }
 
 
