0034f00132a39c8bdd269cbe85222d8f7c209f07
Kerry Washington <mrkerrywashington@icloud.com>
2024-12-21 23:46:09 +0000

    resolved conflict

--- app/src/main/java/com/breadwallet/entities/IntroLanguageResource.kt
+++ app/src/main/java/com/breadwallet/entities/IntroLanguageResource.kt
@@ -24,7 +24,6 @@ class IntroLanguageResource {
                 R.raw.spanish,
                 "¿Estás seguro de que quieres cambiar el idioma a español?",
                 Language.SPANISH,
-<<<<<<< HEAD
             ),
             IntroLanguage(
                 Language.ARABIC.code,
@@ -41,8 +40,6 @@ class IntroLanguageResource {
                 R.raw.french,
                 "Êtes-vous sûr de vouloir changer la langue en français ?",
                 Language.FRENCH
-=======
->>>>>>> main
             ),
             IntroLanguage(
                 Language.INDONESIAN.code,
